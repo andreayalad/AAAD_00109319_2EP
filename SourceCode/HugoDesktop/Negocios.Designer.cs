5273e6771809e67a5815e6693280042b52bd5527
Andrea Ayala <alejandrayala01@gmail.com>
2020-05-27 22:42:49 -0600

    Detalles menores

--- SourceCode/HugoDesktop/Negocios.Designer.cs
+++ SourceCode/HugoDesktop/Negocios.Designer.cs
@@ -29,15 +29,16 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.bt_agregar = new System.Windows.Forms.Button();
+            this.tb_aggdesc = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_agnombre = new System.Windows.Forms.TextBox();
             this.bt_eliminar = new System.Windows.Forms.Button();
             this.tb_eliminar = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.tb_aggdesc = new System.Windows.Forms.TextBox();
-            this.tb_agnombre = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.bt_agregar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,6 +59,7 @@
             this.tableLayoutPanel1.Controls.Add(this.tb_eliminar, 3, 6);
             this.tableLayoutPanel1.Controls.Add(this.label5, 2, 6);
             this.tableLayoutPanel1.Controls.Add(this.label4, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Font = new System.Drawing.Font("Coolvetica Rg", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
@@ -78,30 +80,64 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(864, 464);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
-            // label4
+            // bt_agregar
             // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label4.AutoSize = true;
-            this.tableLayoutPanel1.SetColumnSpan(this.label4, 2);
-            this.label4.Font = new System.Drawing.Font("Coolvetica Rg", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(552, 122);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(192, 29);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Eliminar negocio";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bt_agregar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.bt_agregar.BackColor = System.Drawing.Color.Black;
+            this.tableLayoutPanel1.SetColumnSpan(this.bt_agregar, 2);
+            this.bt_agregar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bt_agregar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bt_agregar.Font = new System.Drawing.Font("Coolvetica Rg", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_agregar.ForeColor = System.Drawing.Color.White;
+            this.bt_agregar.Location = new System.Drawing.Point(3, 354);
+            this.bt_agregar.Name = "bt_agregar";
+            this.bt_agregar.Size = new System.Drawing.Size(426, 33);
+            this.bt_agregar.TabIndex = 0;
+            this.bt_agregar.Text = "A G R E G A R";
+            this.bt_agregar.UseVisualStyleBackColor = false;
+            this.bt_agregar.Click += new System.EventHandler(this.bt_agregar_Click);
             // 
-            // label5
+            // tb_aggdesc
             // 
-            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Coolvetica Rg", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(502, 242);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(76, 22);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Nombre";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.tb_aggdesc.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tb_aggdesc.Font = new System.Drawing.Font("Coolvetica Rg", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_aggdesc.Location = new System.Drawing.Point(243, 281);
+            this.tb_aggdesc.Name = "tb_aggdesc";
+            this.tb_aggdesc.Size = new System.Drawing.Size(162, 23);
+            this.tb_aggdesc.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Coolvetica Rg", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Descripcion";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Coolvetica Rg", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tb_agnombre
+            // 
+            this.tb_agnombre.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tb_agnombre.Font = new System.Drawing.Font("Coolvetica Rg", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_agnombre.Location = new System.Drawing.Point(243, 164);
+            this.tb_agnombre.Name = "tb_agnombre";
+            this.tb_agnombre.Size = new System.Drawing.Size(162, 23);
+            this.tb_agnombre.TabIndex = 13;
             // 
             // bt_eliminar
             // 
@@ -129,64 +165,42 @@
             this.tb_eliminar.Size = new System.Drawing.Size(162, 23);
             this.tb_eliminar.TabIndex = 21;
             // 
-            // label2
+            // label5
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Coolvetica Rg", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(51, 281);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 22);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Descripcion";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Coolvetica Rg", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(502, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 22);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Nombre";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // tb_aggdesc
+            // label4
             // 
-            this.tb_aggdesc.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.tb_aggdesc.Font = new System.Drawing.Font("Coolvetica Rg", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_aggdesc.Location = new System.Drawing.Point(243, 281);
-            this.tb_aggdesc.Name = "tb_aggdesc";
-            this.tb_aggdesc.Size = new System.Drawing.Size(162, 23);
-            this.tb_aggdesc.TabIndex = 14;
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label4, 2);
+            this.label4.Font = new System.Drawing.Font("Coolvetica Rg", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(552, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Eliminar negocio";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // tb_agnombre
+            // label3
             // 
-            this.tb_agnombre.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.tb_agnombre.Font = new System.Drawing.Font("Coolvetica Rg", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_agnombre.Location = new System.Drawing.Point(243, 164);
-            this.tb_agnombre.Name = "tb_agnombre";
-            this.tb_agnombre.Size = new System.Drawing.Size(162, 23);
-            this.tb_agnombre.TabIndex = 13;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Coolvetica Rg", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(70, 164);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 22);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Nombre";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // bt_agregar
-            // 
-            this.bt_agregar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.bt_agregar.BackColor = System.Drawing.Color.Black;
-            this.tableLayoutPanel1.SetColumnSpan(this.bt_agregar, 2);
-            this.bt_agregar.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bt_agregar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.bt_agregar.Font = new System.Drawing.Font("Coolvetica Rg", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bt_agregar.ForeColor = System.Drawing.Color.White;
-            this.bt_agregar.Location = new System.Drawing.Point(3, 354);
-            this.bt_agregar.Name = "bt_agregar";
-            this.bt_agregar.Size = new System.Drawing.Size(426, 33);
-            this.bt_agregar.TabIndex = 0;
-            this.bt_agregar.Text = "A G R E G A R";
-            this.bt_agregar.UseVisualStyleBackColor = false;
-            this.bt_agregar.Click += new System.EventHandler(this.bt_agregar_Click);
+            this.label3.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label3, 2);
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(219, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(426, 39);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "NEGOCIOS ASOCIADOS";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Negocios
             // 
@@ -215,5 +229,6 @@
         private System.Windows.Forms.TextBox tb_agnombre;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button bt_agregar;
+        private System.Windows.Forms.Label label3;
     }
 }
